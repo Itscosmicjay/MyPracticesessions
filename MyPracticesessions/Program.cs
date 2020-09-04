8408d61bbdcfca1a90d49cc43208acea6fb874c9
Itscosmicjay <Itscosmicjay@gmail.com>
2020-09-04 22:07:14 +0100

    First set of variables created along with first Concatenation

--- MyPracticesessions/Program.cs
+++ MyPracticesessions/Program.cs
@@ -13,6 +13,15 @@ namespace MyPracticesessions
             Console.WriteLine(" /  |");
             Console.WriteLine("/___|");
 
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was "+ characterAge +" years old");
+            Console.WriteLine("He loved the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
             Console.ReadLine();
         }
     }
