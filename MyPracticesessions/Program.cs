79c682c451327ec522b3734e17891ff268fc5cf2
Itscosmicjay <Itscosmicjay@gmail.com>
2020-09-04 21:11:39 +0100

    Add project files.

---
+++ MyPracticesessions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyPracticesessions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello World")
+            Console.ReadLine();
+        }
+    }
+}
