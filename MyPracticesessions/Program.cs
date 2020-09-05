0f33dffa53ff2acff0836dba26301edde32b7f1b
Itscosmicjay <Itscosmicjay@gmail.com>
2020-09-05 07:15:21 +0100

    characterName and chracterAge modified

--- MyPracticesessions/Program.cs
+++ MyPracticesessions/Program.cs
@@ -13,9 +13,9 @@ namespace MyPracticesessions
             Console.WriteLine(" /  |");
             Console.WriteLine("/___|");
 
-            string characterName = "John";
+            string characterName = "Tom";
             int characterAge;
-            characterAge = 35;
+            characterAge = 25;
 
             Console.WriteLine("There once was a man named " + characterName);
             Console.WriteLine("He was "+ characterAge +" years old");
