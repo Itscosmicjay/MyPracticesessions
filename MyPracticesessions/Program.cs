6b3ebf19792486b54a1626ce00e2d4fd9d2748af
Itscosmicjay <Itscosmicjay@gmail.com>
2020-09-07 09:38:33 +0100

    Madlibs can be created on C# by storing inputs collected from users in variables

--- MyPracticesessions/Program.cs
+++ MyPracticesessions/Program.cs
@@ -91,7 +91,22 @@ namespace MyPracticesessions
 
             Console.WriteLine(num2+num3);
 
+            string color, pluralNoun, celebrity;
 
+            Console.Write("Enter a color:");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a pluralNoun:");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity:");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+
+            Console.WriteLine(pluralNoun + " are blue");
+
+            Console.WriteLine("I love " + celebrity);
 
             Console.ReadLine();
         }
